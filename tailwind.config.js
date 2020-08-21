0c770b320a637bb78b8160bd3b97747e4ad7acf8
Victor Mustar <victor.mustar@gmail.com>
2020-08-21 17:50:38 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  experimental: {
+    applyComplexClasses: true,
+    uniformColorPalette: true,
+    extendedSpacingScale: true,
+    defaultLineHeights: true,
+    extendedFontSizeScale: true,
+  },
+};
